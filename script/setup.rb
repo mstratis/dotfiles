73a0cda60c21c5a9872f3b198cfd2df90ed7084e
Matt Hodges <hodgesmr1@gmail.com>
2012-12-09 18:51:55 -0500

    starting to piece together the setup script

---
+++ script/setup.rb
@@ -0,0 +1,9 @@
+# Copies in all of the dotfiles
+# Allows you to set up your git name and email
+# Warning: this script is destructive. It will overwrite current dotfiles
+
+# Get the git settings
+
+# Overwrite git settings
+
+# Copy over dotfiles
