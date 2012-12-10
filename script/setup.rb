1cae8b690dac040bc65a6083e7aad36f93c4ab95
Matt Hodges <hodgesmr1@gmail.com>
2012-12-09 19:17:04 -0500

    setup credentials piece

--- script/setup.rb
+++ script/setup.rb
@@ -1,8 +1,24 @@
+#!/usr/bin/ruby
+
 # Copies in all of the dotfiles
 # Allows you to set up your git name and email
 # Warning: this script is destructive. It will overwrite current dotfiles
 
+@git_name = nil;
+@git_email = "nil;
+
 # Get the git settings
+def read_git_credentials
+
+  print "Enter your full name: "
+  @git_name = gets
+  @git_name.chomp!
+  
+  print "Enter your email address: "
+  @git_email = gets
+  @git_email.chomp!
+
+end
 
 # Overwrite git settings
 
